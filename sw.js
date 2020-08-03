1a369fe9a2db33e8e4af8a4c7e83f63eab598756
王军豪 <479068012@qq.com>
2020-08-03 22:15:03 +0800

    Site updated: 2020-08-03 22:15:03

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@ workbox.setConfig({ modulePathPrefix: 'https://g.alicdn.com/kg/workbox/3.3.0/' }
 self.addEventListener('install', function (event) {
   event.waitUntil(
     caches.keys().then(function (names) {
-      var validSets = ["is-sw-94bb61","is-html-94bb61","is-jsdelivr-94bb61","is-theme-94bb61","is-json-94bb61"];
+      var validSets = ["is-sw-38d7c5","is-html-38d7c5","is-jsdelivr-38d7c5","is-theme-38d7c5","is-json-38d7c5"];
       return Promise.all(
         names
           .filter(function (name) { return !~validSets.indexOf(name); })
@@ -18,18 +18,18 @@ self.addEventListener('install', function (event) {
 });
 
 workbox.routing.registerRoute(new RegExp('sw\\.js'), workbox.strategies.networkOnly({
-  cacheName: 'is-sw-94bb61',
+  cacheName: 'is-sw-38d7c5',
 }));
 workbox.routing.registerRoute(new RegExp('https://cdn\\.jsdelivr\\.net'), workbox.strategies.staleWhileRevalidate({
-  cacheName: 'is-jsdelivr-94bb61',
+  cacheName: 'is-jsdelivr-38d7c5',
   plugins: [ new workbox.expiration.Plugin({ maxAgeSeconds: 14400 }) ],
 }));
 workbox.routing.registerRoute(new RegExp('/.*\\.(?:js|css|woff2|png|jpg|gif)$'), workbox.strategies.staleWhileRevalidate({
-  cacheName: 'is-theme-94bb61',
+  cacheName: 'is-theme-38d7c5',
   plugins: [ new workbox.expiration.Plugin({ maxAgeSeconds: 14400 }) ],
 }));
 workbox.routing.registerRoute(new RegExp('your_data_prefix/.*\\.json'), workbox.strategies.cacheFirst({
-  cacheName: 'is-json-94bb61',
+  cacheName: 'is-json-38d7c5',
   plugins: [ new workbox.expiration.Plugin({ maxAgeSeconds: 14400 }) ],
 }));
 
